f7f8947435bbd12429588fb82c7703645a25a07a
Kaihanahammed <kaihanahammed07@gmail.com>
2023-09-12 12:48:45 +0600

    Thirteenth commit.

--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav_bar").style.top = "0";
+    document.getElementById("nav_bar").hidden=false;
   } else {
     var width_of_nav_bar = 
     document.getElementById("nav_bar").scrollWidth;
-    document.getElementById("nav_bar").style.top = "-"+width_of_nav_bar+"px";
+    //document.getElementById("nav_bar").style.top = "-"+width_of_nav_bar+"px";
+    document.getElementById("nav_bar").hidden=true;
   }
   prevScrollpos = currentScrollPos;
 }
